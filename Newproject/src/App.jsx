0af01f75e8244111a98c4ffcd76b733343fd9c33
Jigneshkalal <jignehkalal67@gmail.com>
2024-06-24 12:28:14 +0530

    learn componet inject

---
+++ Newproject/src/App.jsx
@@ -0,0 +1,20 @@
+import { Footer } from "./Footer"
+import { Header } from "./Header"
+ import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus tempore nisi dicta omnis tenetur necessitatibus ut praesentium dolores harum sit, dolor quibusdam illum veritatis eum pariatur recusandae dolore cupiditate hic?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus tempore nisi dicta omnis tenetur necessitatibus ut praesentium dolores harum sit, dolor quibusdam illum veritatis eum pariatur recusandae dolore cupiditate hic?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus tempore nisi dicta omnis tenetur necessitatibus ut praesentium dolores harum sit, dolor quibusdam illum veritatis eum pariatur recusandae dolore cupiditate hic?</p>
+
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Newproject/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <p className='Color'>Hello i am Header</p>
+    </div>
+  )
+}
