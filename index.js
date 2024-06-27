bdff960c0c9c3c7b0688a0654cef76d0a79ccc5e
Jigneshkalal <jignehkalal67@gmail.com>
2024-06-27 12:58:31 +0530

    jS And React classes

---
+++ index.js
@@ -0,0 +1,16 @@
+class ClassName {
+  constructor(name,year) {
+    this.name=name
+    this.year=year
+   }
+   run(){
+    console.log("This is a",this.name,this.year);
+   }
+   Speed(s){
+    console.log("it's Top Speed is",s);
+   }
+}
+
+const mycar1=new ClassName("BMW M5",2024);
+ mycar1.run()
+ mycar1.Speed(400)
